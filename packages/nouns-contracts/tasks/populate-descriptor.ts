8275e8d23ff4e0a4ff0c342be1d5c71c167dbdeb
Keon <kwk236@gmail.com>
2022-08-17 02:56:47 +0900

    verify & populate descriptors

--- packages/nouns-contracts/tasks/populate-descriptor.ts
+++ packages/nouns-contracts/tasks/populate-descriptor.ts
@@ -6,13 +6,13 @@ task('populate-descriptor', 'Populates the descriptor with color palettes and No
   .addOptionalParam(
     'nftDescriptor',
     'The `NFTDescriptor` contract address',
-    '0xB29C9d34f736Ce78EceC7EFB964Feb3508B79dA9',
+    '0x0fd3Fb251E1112e46efD8c0da199E6245FaE1F77',
     types.string,
   )
   .addOptionalParam(
     'nounsDescriptor',
     'The `NounsDescriptor` contract address',
-    '0x6994AE600820e78a9AA1E45268efaAC114367D5E',
+    '0x1197Ec597045403bBae28B72488eB952C71705F3',
     types.string,
   )
   .setAction(async ({ nftDescriptor, nounsDescriptor }, { ethers }) => {
@@ -27,44 +27,33 @@ task('populate-descriptor', 'Populates the descriptor with color palettes and No
     const { bodies, ears, heads, glasses, faces } = images;
 
     // Chunk head and accessory population due to high gas usage
-    // console.log("adding bacground")
-    // await descriptorContract.addManyBackgrounds(bgcolors);
-    // await new Promise(f => setTimeout(f, 5000));
+    console.log("adding bacground")
+    await descriptorContract.addManyBackgrounds(bgcolors);
+    await new Promise(f => setTimeout(f, 10000));
 
-    // console.log("adding palette")
-    // await descriptorContract.addManyColorsToPalette(0, palette);
-    // await new Promise(f => setTimeout(f, 5000));
+    console.log("adding palette")
+    await descriptorContract.addManyColorsToPalette(0, palette);
+    await new Promise(f => setTimeout(f, 10000));
 
-    // console.log("adding bodies")
-    // await descriptorContract.addManyBodies(bodies.map(({ data }) => data));
-    // await new Promise(f => setTimeout(f, 5000));
+    console.log("adding bodies")
+    await descriptorContract.addManyBodies(bodies.map(({ data }) => data));
+    await new Promise(f => setTimeout(f, 10000));
 
-    // console.log("adding ears")
-    // await descriptorContract.addManyEars(ears.map(({ data }) => data));
-    // await new Promise(f => setTimeout(f, 5000));
-    // const earChunk = chunkArray(ears, 10);
-    // for (const chunk of earChunk) {
-    //   await descriptorContract.addManyEars(chunk.map(({ data }) => data));
-    //   await new Promise(f => setTimeout(f, 5000));
-    // }
-
+    console.log("adding ears")
+    await descriptorContract.addManyEars(ears.map(({ data }) => data));
+    await new Promise(f => setTimeout(f, 10000));
 
     console.log("adding heads")
     await descriptorContract.addManyHeads(heads.map(({ data }) => data));
-    await new Promise(f => setTimeout(f, 5000));
-    // const headChunk = chunkArray(heads, 10);
-    // for (const chunk of headChunk) {
-    //   await descriptorContract.addManyHeads(chunk.map(({ data }) => data));
-    //   await new Promise(f => setTimeout(f, 5000));
-    // }
+    await new Promise(f => setTimeout(f, 10000));
 
     console.log("adding glasses")
     await descriptorContract.addManyGlasses(glasses.map(({ data }) => data));
-    await new Promise(f => setTimeout(f, 5000));
+    await new Promise(f => setTimeout(f, 10000));
 
     console.log("adding faces")
     await descriptorContract.addManyFaces(faces.map(({ data }) => data));
-    await new Promise(f => setTimeout(f, 5000));
+    await new Promise(f => setTimeout(f, 10000));
 
     console.log('Descriptor populated with palettes and parts');
   });
