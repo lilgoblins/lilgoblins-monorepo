936464fa7acb0ce1609529df2f0cb301f06e1d6f
Keon <kwk236@gmail.com>
2022-08-17 06:15:39 +0900

    update auction acitivity wrapper

--- packages/nouns-webapp/src/components/AuctionActivityWrapper/index.tsx
+++ packages/nouns-webapp/src/components/AuctionActivityWrapper/index.tsx
@@ -2,6 +2,6 @@ import classes from './AuctionActivityWrapper.module.css';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const AuctionActivityWrapper: React.FC<{}> = props => {
-  return <div className={classes.wrapper} style={{backgroundColor: "rgba(177, 177, 177, 0.5)", padding:"6px", borderRadius: "12px"}}>{props.children}</div>;
+  return <div className={classes.wrapper} style={{backgroundColor: "rgba(225, 225, 225, 0.85)", padding:"6px", borderRadius: "12px"}}>{props.children}</div>;
 };
 export default AuctionActivityWrapper;
